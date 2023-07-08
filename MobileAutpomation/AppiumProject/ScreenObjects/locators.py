1aca3d2e14cd244546c22fd6d28916f63aa82dc9
saikiranshet <32921574+saikiranshet@users.noreply.github.com>
2023-07-08 16:31:53 +0530

    Required Files

---
+++ MobileAutpomation/AppiumProject/ScreenObjects/locators.py
@@ -0,0 +1,11 @@
+class locators(object):
+    EnterSomeValue = "com.code2lead.kwad:id/EnterValue"
+    ContactUs = "com.code2lead.kwad:id/ContactUs"
+    ScrollView = "com.code2lead.kwad:id/ScrollView"
+    TabView = "com.code2lead.kwad:id/TabView"
+    Zoom = "com.code2lead.kwad:id/Zoom"
+    LoginMainScreen = "com.code2lead.kwad:id/Login"
+    email = "com.code2lead.kwad:id/Et4"
+    password = "com.code2lead.kwad:id/Et5"
+    loginButton = "com.code2lead.kwad:id/Btn3"
+    LongClick = "com.code2lead.kwad:id/LongClick"
